ea502f5df28f423ea5be3118db76a0e939fcee12
helansuhana <helensuhanaarul@gmail.com>
2026-04-19 19:15:44 +0530

    Use deployed backend URL for frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://task-manager-6ras.onrender.com";
 
 const messageEl = document.getElementById("message");
 const taskListEl = document.getElementById("task-list");
